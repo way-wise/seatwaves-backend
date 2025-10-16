5e013c29619848c6dab3d01a81307806b712ef65
MD. ARIFUL ISLAM <arif171042@gmail.com>
2025-10-16 21:11:14 +0600

    update queyr

--- src/event/dto/query.dto.ts
+++ src/event/dto/query.dto.ts
@@ -5,7 +5,7 @@ export const queryEventSchema = z.object({
   limit: z.string().optional(),
   page: z.string().optional(),
   cursor: z.string().optional(),
-  sortBy: z.enum(['price', 'createdAt', 'updatedAt']).optional(),
+  sortBy: z.enum(['startTime', 'createdAt', 'updatedAt']).optional(),
   sortOrder: z.enum(['asc', 'desc']).optional(),
   isActive: z.enum(['true', 'false']).optional(),
 });
