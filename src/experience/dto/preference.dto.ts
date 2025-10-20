3c69e73d252671cbf95c5641c0dded1ca094f688
MD. ARIFUL ISLAM <arif171042@gmail.com>
2025-10-20 15:08:12 +0600

    clean code

--- src/experience/dto/preference.dto.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-export const preferenceSchema = z.object({
-  amenityIds: z.array(z.string()).min(1, 'At least one amenity is required'),
-  agreement: z.string({ required_error: 'Agreement is required' }).url(),
-});
